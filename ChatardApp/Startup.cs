9f9d782cff591ce7e93fbf4bea82145ab5f31500
Patrick Haimbaugh <Phaimbaugh2000@gmail.com>
2017-01-24 12:56:07 -0500

    Add project files.

---
+++ ChatardApp/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(ChatardApp.Startup))]
+namespace ChatardApp
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
