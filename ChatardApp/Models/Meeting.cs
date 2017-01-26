f41bce89d77d6ce04d595b9b800c6667b2646658
Patrick Haimbaugh <Phaimbaugh2000@gmail.com>
2017-01-26 14:53:59 -0500

    Created Meeting class

---
+++ ChatardApp/Models/Meeting.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace ChatardApp.Models
+{
+    public class Meeting
+    {
+        public int Id { get; set; }
+
+        [Required]
+        public ApplicationUser Coach { get; set; }
+
+        public DateTime DateTime { get; set; }
+
+        [Required]
+        [StringLength(255)]
+        public string Venue { get; set; }
+
+        [Required]
+        public Event Event { get; set; }
+    }
+}
