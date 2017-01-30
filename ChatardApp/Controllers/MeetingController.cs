ae408704e1c4e8399ce5ecc2eacb9e44b45b2da5
Patrick Haimbaugh <Phaimbaugh2000@gmail.com>
2017-01-30 10:57:58 -0500

    Created Add Event Form

--- ChatardApp/Controllers/MeetingController.cs
+++ ChatardApp/Controllers/MeetingController.cs
@@ -1,17 +1,27 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using ChatardApp.Models;
+using ChatardApp.ViewModels;
 using System.Linq;
-using System.Web;
 using System.Web.Mvc;
 
 namespace ChatardApp.Controllers
 {
     public class MeetingController : Controller
     {
-        // GET: Meeting
-        public ActionResult Create()
-        {
-            return View();
+
+            private readonly ApplicationDbContext _context;
+
+            public MeetingController()
+            {
+                _context = new ApplicationDbContext();
+            }
+
+            public ActionResult Create()
+            {
+                var viewModel = new MeetingFormViewModel
+                {
+                    Events = _context.Events.ToList()
+                };
+                return View(viewModel);
+            }
         }
-    }
 }
---
+++ ChatardApp/ViewModels/MeetingFormViewModel.cs
@@ -0,0 +1,14 @@
+﻿using ChatardApp.Models;
+using System.Collections.Generic;
+
+namespace ChatardApp.ViewModels
+{
+    public class MeetingFormViewModel
+    {
+        public string Venue { get; set; }
+        public string Date { get; set; }
+        public string Time { get; set; }
+        public int Event { get; set; }
+        public IEnumerable<Event> Events { get; set; }
+    }
+}
